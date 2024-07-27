cbf7216455904679763b3115e0ee075522e2d357
Mário-FG-Miranda <mariogomesmiranda189@gmail.com>
2024-07-27 11:35:58 -0300

    meu terceiro commit

--- src/desaio.js
+++ src/desaio.js
@@ -1,4 +1,13 @@
-let x = 5
-let y = 5
-let resultado = x * y 
-console.log(resultado)
+let nomeHeroi = "Jesus"
+let xpferro = 1000
+let xpBronze = 1001 <=2000
+
+switch(xpBronze){
+case 1000:
+    console.log("O Heroi","" + nomeHeroi,"está no nivel de" , "" + xpBronze)
+    break
+
+    case 1500:
+        console.log("O Heroi", "" + nomeHeroi, "está no nivel de", "" + xpBronze)
+
+}
